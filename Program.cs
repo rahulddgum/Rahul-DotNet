15f7421b6457bb05641002254a26b4cd58b73760
rahulddgum <rahul.didigum@gmail.com>
2016-09-25 15:42:55 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dynamic
+{
+    public static class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Size of int: {0}", sizeof(int));
+            Console.WriteLine("Size of int: {0}", sizeof(float));
+            Console.WriteLine("Size of int: {0}", sizeof(char));
+            Console.WriteLine("Size of int: {0}", sizeof(double));
+            Console.ReadLine();
+
+        }
+    }
+}
